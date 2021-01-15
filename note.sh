c95ef12698f269bdf06e117e44e3ec1571d07eb2
Eyad Ali <51314695+eyadali05@users.noreply.github.com>
2021-01-15 16:19:30 +0200

    Added the script

---
+++ note.sh
@@ -0,0 +1,23 @@
+#TODO 
+ #. get 1 parameter, filename
+ #. open in neovim
+ #. maybe even making a keybinding to the notes
+
+Note() {
+	dt=$(date '+%d/%m/%Y %H:%M:%S')
+	cd '/home/eyadali05/Notes'
+	mkdir $1
+	cd $1
+	NOTEFILENAME="$1.md"
+	touch $NOTEFILENAME
+	nvim -c "norm Go" \
+		-c "norm Go-- $1 - Note --" \
+		-c "norm Go## $(date +%d/%m/%Y)" \
+		-c "norm Go# $(date +%H:%M:%S)" \
+		-c "norm G2o" \
+		-c "norm zz" \
+		-c "startinsert" $NOTEFILENAME
+	
+}
+
+Note $1
